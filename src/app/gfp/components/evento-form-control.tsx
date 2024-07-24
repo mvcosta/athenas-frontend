9cd8215f2bf3a1fcb2cb4ac217ceacfa369e3b8e
Marcus Vinicius <mvcoosta@gmail.com>
2024-07-24 19:30:01 -0400

    Adiciona busca na barra de eventos

--- src/app/gfp/components/evento-form-control.tsx
+++ src/app/gfp/components/evento-form-control.tsx
@@ -7,10 +7,16 @@ import {
   InputGroup,
   InputLeftAddon,
   Input,
+  Popover,
+  PopoverBody,
+  PopoverContent,
+  PopoverTrigger,
 } from "@chakra-ui/react";
 import SelectEvento from "./select-evento";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Evento } from "@/models/eventos.models";
+import { useQuery } from "@tanstack/react-query";
+import { searchEventosQuery } from "../eventos/lib/eventos";
 
 export default function EventoFormControl() {
   const [value, setValue] = useState<string>("");
@@ -18,20 +24,67 @@ export default function EventoFormControl() {
   const handleSelectedEvento = (evento: Evento) =>
     setValue(`${evento.numero} (${evento.rubrica}) - ${evento.titulo}`);
 
+  const initialFocusRef = useRef<any>();
+
   return (
     <Box>
-      <FormLabel>Evento:</FormLabel>
-      <InputGroup>
-        <InputLeftAddon>
-          <SearchIcon />
-        </InputLeftAddon>
-        <Input
-          value={value}
-          onChange={handleChange}
-          placeholder="Busque o evento"
-        />
-        <SelectEvento onSelected={handleSelectedEvento} />
-      </InputGroup>
+      <Popover initialFocusRef={initialFocusRef} placement="bottom-start">
+        <FormLabel>Evento:</FormLabel>
+        <InputGroup>
+          <InputLeftAddon>
+            <SearchIcon />
+          </InputLeftAddon>
+          <PopoverTrigger>
+            <Input
+              ref={initialFocusRef}
+              value={value}
+              onChange={handleChange}
+              placeholder="Busque o evento"
+            />
+          </PopoverTrigger>
+          <SelectEvento onSelected={handleSelectedEvento} />
+        </InputGroup>
+        {value ? (
+          <SearchBox search={value} onClick={handleSelectedEvento} />
+        ) : null}
+      </Popover>
     </Box>
+  );
+}
+
+function SearchBox({
+  search,
+  onClick,
+}: {
+  search: string;
+  onClick: (evento: Evento) => void;
+}) {
+  const { data } = useQuery({
+    queryKey: ["eventos", { search: search }],
+    queryFn: searchEventosQuery,
+  });
+
+  const showContent = data ? data.length > 0 : false;
+
+  return (
+    <>
+      {showContent ? (
+        <PopoverContent width="auto">
+          <PopoverBody>
+            {data?.map((e) => (
+              <Box
+                key={e.id}
+                cursor="pointer"
+                padding="1"
+                _hover={{ bg: "#152838" }}
+                onClick={() => onClick(e)}
+              >
+                {e.numero} ({e.rubrica}) - {e.titulo}
+              </Box>
+            ))}
+          </PopoverBody>
+        </PopoverContent>
+      ) : null}
+    </>
   );
 }
--- src/app/gfp/eventos/lib/eventos.ts
+++ src/app/gfp/eventos/lib/eventos.ts
@@ -2,8 +2,6 @@ import { authAPIPaginatedFetch } from "@/lib/fetch";
 import { Evento, EventoResponse } from "@/models/eventos.models";
 import { QueryFunctionContext } from "@tanstack/react-query";
 
-type QueryKey = [string, { page: number; limit: number }];
-
 export async function getEventos(
   page: number = 0,
   limit: number = 10
@@ -16,15 +14,25 @@ export async function getEventos(
 
 export async function getEventosQuery({
   queryKey,
-}: QueryFunctionContext<QueryKey>): Promise<{
+}: QueryFunctionContext<[string, { page: number; limit: number }]>): Promise<{
   eventos: Evento[];
   count: number;
 }> {
-  console.log("fetching");
-
   const [_, { page, limit }] = queryKey;
   const response = await authAPIPaginatedFetch("eventos/", page, limit);
 
   const eventoResponse: EventoResponse = await response.json();
   return { eventos: eventoResponse.results, count: eventoResponse.count };
+}
+
+export async function searchEventosQuery({
+  queryKey,
+}: QueryFunctionContext<[string, { search: string }]>): Promise<Evento[]> {
+  const [_, { search }] = queryKey;
+  const query = search ? `eventos/?search=${search}` : "eventos";
+  const response = await authAPIPaginatedFetch(query, 1, 20);
+
+  const eventoResponse: EventoResponse = await response.json();
+
+  return eventoResponse.results;
 }
